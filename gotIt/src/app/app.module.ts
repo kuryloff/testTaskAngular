65be1e01e1fa089a3e5b295baad7caf4955a1665
Ihor Kurylov <iikurylov@gmail.com>
2020-07-13 19:18:50 +0300

    Hours for appointment section done +0.75 hr
    Total 4.25 hrs

--- gotIt/src/app/app.module.ts
+++ gotIt/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FooterComponent } from './Components/footer/footer.component';
 import { MainComponent } from './Components/main/main.component';
 import { BackgroundComponent } from './Components/background/background.component';
 import { PharmacyComponent } from './Components/pharmacy/pharmacy.component';
+import { HoursComponent } from './Components/hours/hours.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { PharmacyComponent } from './Components/pharmacy/pharmacy.component';
     FooterComponent,
     MainComponent,
     BackgroundComponent,
-    PharmacyComponent
+    PharmacyComponent,
+    HoursComponent
   ],
   imports: [
     BrowserModule,
